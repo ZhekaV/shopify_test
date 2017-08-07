d8ee7113aab3e5d4ddf1c87a4287857673349309
zhekav <zhekavigura@gmail.com>
2017-08-07 11:29:36 +0300

    no message

--- app/services/import_products.rb
+++ app/services/import_products.rb
@@ -13,8 +13,29 @@ class ImportProducts
 
   attr_reader :shop
 
-  # TODO: paginate products
+  # TODO: paginate resources
+  # TODO: rescue 429
+  # TODO: replace it with plain json request for speed & memory improvements
+  def shopify_request(resources_kind)
+    ShopifyAPI::Session.temp(
+      shop.shopify_domain,
+      shop.shopify_token
+    ) { "ShopifyAPI::#{resources_kind}".constantize.find(:all, params: { limit: 250 }) }
+  end
+
   def products
-    @products ||= ShopifyAPI::Session.temp(shop.shopify_domain, shop.shopify_token) { ShopifyAPI::Product.find(:all, params: { limit: 50 }) }
+    shopify_request('Product')
+  end
+
+  def collects
+    shopify_request('Collect')
+  end
+
+  def custom_collections
+    shopify_request('CustomCollection')
+  end
+
+  def smart_collections
+    shopify_request('SmartCollection')
   end
 end
