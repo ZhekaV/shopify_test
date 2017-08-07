d34e7ffc64020c6a30d2187546cc8ee27af389b7
zhekav <zhekavigura@gmail.com>
2017-08-07 23:06:52 +0300

    refactoring

--- app/services/concerns/save_collections.rb
+++ app/services/concerns/save_collections.rb
@@ -3,29 +3,28 @@ module SaveCollections
 
   def save_custom_collections
     @current_kind = :custom
-    custom_collections.each do |collection|
-      @current_collection = collection
-      @persisted_collection = Collection.find_by(shopify_id: current_collection.id)
-      next if persisted_collection && persisted_collection.updated_at > current_collection.updated_at
-      proccess_collection
-    end
+    proccess_collections(custom_collections)
   end
 
   def save_smart_collections
     @current_kind = :smart
-    smart_collections.each do |collection|
-      @current_collection = collection
-      @persisted_collection = Collection.find_by(shopify_id: current_collection.id)
-      next if persisted_collection && persisted_collection.updated_at > current_collection.updated_at
-      proccess_collection
-    end
+    proccess_collections(smart_collections)
   end
 
   private
 
   attr_reader :current_kind, :current_collection, :persisted_collection
 
-  def proccess_collection
+  def proccess_collections(collections)
+    collections.each do |collection|
+      @current_collection = collection
+      @persisted_collection = Collection.find_by(shopify_id: current_collection.id)
+      next if persisted_collection && persisted_collection.updated_at > current_collection.updated_at
+      create_collection
+    end
+  end
+
+  def create_collection
     persisted_collection.delete if persisted_collection.present?
     Collection.create(
       current_collection.attributes.slice(:kind, :handle, :title, :body_html, :sort_order, :template_suffix, :published_scope).merge(
